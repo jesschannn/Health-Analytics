72aa62f23ad842e2cb07feb9be0e89e5f7cdf658
jesschannn <jessica.chan.3@stonybrook.edu>
2023-08-17 01:46:00 +0000

    added if,then statements

--- health-analytics.py
+++ health-analytics.py
@@ -14,3 +14,30 @@ varDictionary = {
         'emergencycontact3' : 'sheila jackson'
     }
 }
+
+oxygenSaturation = 89
+respiratoryRate = 22
+if oxygenSaturation >=95 and respiratoryRate <12:
+    print('You do not need oxygen treatment, your percentage is at: ', oxygenSaturation)
+    print('Your respiratory rate is low at: ', respiratoryRate)
+elif oxygenSaturation >=95 and respiratoryRate >=12 and respiratoryRate <=20:
+    print('You do not need oxygen treatment, your percentage is at: ', oxygenSaturation)
+    print('Your respiratory rate is normal at: ', respiratoryRate)
+elif oxygenSaturation >=95 and respiratoryRate >20: 
+    print('You do not need oxygen treatment, your percentage is at: ', oxygenSaturation)
+    print ('Your respiratory rate is high at: ', respiratoryRate)
+elif oxygenSaturation <=94 and respiratoryRate <12:
+    print('You need oxygen treatment, your percentage is at: ', oxygenSaturation)
+    percentageUntilNormal = 95 - oxygenSaturation
+    print('Your oxygen saturation level is this percent away from normal: ', percentageUntilNormal)
+    print('Your respiratory rate is low at: ', respiratoryRate)
+elif oxygenSaturation <=94 and respiratoryRate >=12 and respiratoryRate <=20:
+    print('You need oxygen treatment, your percentage is at: ', oxygenSaturation)
+    percentageUntilNormal = 95 - oxygenSaturation
+    print('Your oxygen saturation level is this percent away from normal: ', percentageUntilNormal)
+    print('Your respiratory rate is normal at: ', respiratoryRate)
+else: 
+    print('You need oxygen treatment, your percentage is at: ', oxygenSaturation)
+    percentageUntilNormal = 95 - oxygenSaturation
+    print('Your oxygen saturation level is this percent away from normal: ', percentageUntilNormal)
+    print('Your respiratory rate is high at: ', respiratoryRate) 
