d925cde8505416883dff1d6bb8bc52e2525bc955
jesschannn <jessica.chan.3@stonybrook.edu>
2023-08-17 00:34:13 +0000

    added variables

--- health-analytics.py
+++ health-analytics.py
@@ -1,3 +1,16 @@
 import pandas as pd  
 import numpy as np  
 
+varNumber = 90
+varString = 'breathing information'
+varList = ['o2 level', 'respiration rate', 'lung capacity']
+varDictionary = {
+    "patient" : "Frank Gallagher",
+    "age" : "50",
+    "symptoms" : ['cough', 'dizziness', 'headache'],
+    "emergencycontact" : {
+        'emergencycontact1' : 'fiona gallagher',
+        'emergencycontact2' : 'philip gallagher',
+        'emergencycontact3' : 'sheila jackson'
+    }
+}
